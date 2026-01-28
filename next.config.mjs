9ae45d79de2fac52e3a40283c97caa5c84ae9487
Cory Miller <corydhmiller@gmail.com>
2026-01-28 08:55:07 -0600

    Replace CF support with Netlify (#4)
    * update packages
    
    * eslint config
    
    * remove all cloudflare specific stuff
    
    * chore: update lockfile for CI
    
    * remove cloudflare
    
    * re-install

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 };
-
-if (process.env.NODE_ENV === "development") {
-  await initOpenNextCloudflareForDev();
-}
 
 export default nextConfig;
